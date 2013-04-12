26b753a61275010230bea5d329a67e0b3956877b
Ulrich Kautz <uk@fortrabbit.de>
2013-04-12 18:47:54 +0200

    First draft of phar creation

--- src/Pharify/Commands/CreateCommand.php
+++ src/Pharify/Commands/CreateCommand.php
@@ -25,6 +25,42 @@ class CreateCommand extends AbstractCommand
 {
 
     /**
+     * @var string
+     */
+    protected $pharName;
+
+    /**
+     * @var string
+     */
+    protected $pharFile;
+
+    /**
+     * @var string
+     */
+    protected $outputDir;
+
+    /**
+     * @var string
+     */
+    protected $stubFile;
+
+    /**
+     * @var array
+     */
+    protected $includes;
+
+    /**
+     * @var string
+     */
+    protected $currentDir;
+
+    /**
+     * @var \Phar
+     */
+    protected $phar;
+
+
+    /**
      * Init args
      */
     public function init()
@@ -33,18 +69,68 @@ class CreateCommand extends AbstractCommand
             ->setName('create')
             ->setDescription('Create a new PHAR')
             ->addArgument('name', Argument::REQUIRED, null, 'Name of the phar output file, eg "foo" creates "foo.phar"')
+            ->addArgument('includes', Argument::OPTIONAL, null, 'Comma separated list of relative paths to files or directories to include. Defaults to current directory.')
             ->addArgument('directory', Argument::OPTIONAL, null, 'Output folder. Defaults to current dir.')
-            ->addArgument('stub', Argument::OPTIONAL, null, 'Optional stub file.');
+            ->addArgument('stub', Argument::OPTIONAL, null, 'Optional stub file. Falls back default stub.');
     }
 
 
     /**
-     *
+     * @throws \Exception
      */
     public function execute()
     {
-        $name      = $this->getArgument('name');
-        $outputDir = $this->hasArgument('directory') ? $this->getArgument('directory') : getcwd();
-        print_r(['NAME' => $name, 'DIR' => $outputDir]);
+        $this->pharName   = $this->getArgument('name');
+        $this->pharFile   = $this->pharName. '.phar';
+        $this->currentDir = getcwd();
+        $this->outputDir  = $this->hasArgument('directory') ? $this->getArgument('directory') : $this->currentDir;
+        $this->stubFile   = $this->hasArgument('stub') ? $this->getArgument('stub') : null;
+        $this->includes   = $this->hasArgument('includes') ? preg_split('/\s*,\s*/', $this->getArgument('includes')) : [$this->currentDir];
+        $this->phar       = new \Phar($this->outputDir. '/'. $this->pharFile, 0, $this->pharFile);
+
+        foreach ($this->includes as $path) {
+            $absPath = realpath($path);
+            if (strpos($absPath, $this->currentDir) !== 0) {
+                throw new \Exception("Could not find '$path' in current directory");
+            }
+            $this->addPharFiles($absPath);
+        }
+
+        if (!is_null($this->stubFile)) {
+            $this->phar->setStub($this->phar->createDefaultStub($this->stubFile));
+        } else {
+            $this->phar->setStub("#!/usr/bin/env php\n<?php\nPhar::mapPhar('". $this->pharFile. "');\n__HALT_COMPILER();");
+        }
+        $this->phar->stopBuffering();
+
     }
+
+    /**
+     * The long description
+     *
+     * @param string  $dir   Directory to look in
+     */
+    protected function addPharFiles($path)
+    {
+        if (is_file($path)) {
+            $relPath = substr($path, strlen($this->currentDir)+ 1);
+            $this->phar->addFromString($relPath, file_get_contents($path));
+        } elseif (is_dir($path)) {
+            if (($dh = opendir($path)) !== false) {
+                while (($p = readdir($dh)) !== false) {
+                    if ($p === '.' || $p === '..' || "$path/$p" == $this->outputDir. '/'. $this->pharFile) {
+                        continue;
+                    }
+                    $this->addPharFiles("$path/$p");
+                }
+                closedir($dh);
+            }
+        }
+    }
+
 }
+
+
+/*
+<?php __HALT_COMPILER();
+*/
