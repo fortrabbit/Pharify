19d356bde19088fafe8feb03e0076a6bec94d81b
Ulrich Kautz <uk@fortrabbit.de>
2013-06-18 12:12:59 +0200

    Support of .pharify files

--- stub.php
+++ stub.php
@@ -6,6 +6,16 @@ Phar::mapPhar("pharify.phar");
 include_once "phar://pharify.phar/vendor/autoload.php";
 
 $pharify = new Pharify\Console();
-$pharify->run();
+
+
+if (file_exists(".pharify")) {
+    $newArgv = $argv;
+    array_shift($newArgv);
+    $newArgv = array_merge($newArgv, preg_split('/\s+/', file_get_contents(".pharify")));
+    $input = new Symfony\Component\Console\Input\ArgvInput($newArgv);
+    $pharify->run($input);
+} else {
+    $pharify->run();
+}
 
 __HALT_COMPILER();
